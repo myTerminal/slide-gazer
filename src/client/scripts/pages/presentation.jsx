d33aba4c19ac0d46a38445e44d73263a1a17b619
myTerminal <team.terminal@gmail.com>
2018-01-26 16:31:09 +0000

    styled most of presentation-page

--- src/client/scripts/pages/presentation.jsx
+++ src/client/scripts/pages/presentation.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-    NavLink,
-    Redirect
-} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 export default class Presentation extends React.Component {
 
@@ -24,6 +21,24 @@ export default class Presentation extends React.Component {
         });
     }
 
+    reloadLastPresentation () {
+        this.setState({
+            isPresentationLoaded: true
+        });
+    }
+
+    toggleAutoTransition () {
+        this.setState({
+            isAutoTransitionEnabled: !this.state.isAutoTransitionEnabled
+        });
+    }
+
+    setAnimation (animation) {
+        this.setState({
+            animation: animation
+        });
+    }
+
     render () {
         if (this.state.redirectToHome) {
             return <Redirect to='/' />;
@@ -37,38 +52,53 @@ export default class Presentation extends React.Component {
                   <div id='top-panel-progress-bar'></div>
                 </div>
                 <div id='top-panel-body'>
-                  <div id='presentation-controls'>
-                    <div className='controls-button' id='auto-progress' onClick={this.backToHome.bind(this)}>
+                  <div className='controls-row'>
+                    <div className='controls-row-header'>
+                      Misc
+                    </div>
+                    <div className={'control-button ' + (!this.state.isPresentationLoaded ? 'hidden' : '')} onClick={this.backToHome.bind(this)}>
+                      End Presentation
+                    </div>
+                    <div className={'control-button ' + (this.state.isPresentationLoaded ? 'hidden' : '')} onClick={this.backToHome.bind(this)}>
+                      Back
+                    </div>
+                  </div>
+                  <div className='controls-row'>
+                    <div className='controls-row-header'>
+                      Transition
+                    </div>
+                    <div className={'control-button ' + (this.state.isAutoTransitionEnabled ? 'active' : '')} onClick={this.toggleAutoTransition.bind(this)}>
                       Auto
                     </div>
-                    <div className='controls-separator'></div>
-                    <div className='controls-button animation-control' data-animation='fade'>
-                      Fade
+                  </div>
+                  <div className='controls-row'>
+                    <div className='controls-row-header'>
+                      Animation
                     </div>
-                    <div className='controls-button animation-control' data-animation='slide-up'>
-                      Slide-up
-                    </div>
-                    <div className='controls-button animation-control' data-animation='unfold-down'>
-                      Unfold-down
-                    </div>
-                    <div className='controls-button animation-control' data-animation='unfold-up'>
-                      Unfold-up
-                    </div>
-                    <div className='controls-button animation-control' data-animation='zoom'>
-                      Zoom
-                    </div>
-                    <div className='controls-button animation-control' data-animation='flip'>
-                      Flip
-                    </div>
+                    {
+                      ['fade', 'slide-up', 'unfold-down', 'unfold-up', 'zoom', 'flip'].map(animation => {
+                        return (
+                          <div key={animation} className={'control-button ' + (this.state.animation === animation ? 'active' : '')} onClick={() => this.setAnimation.bind(this)(animation)}>
+                            { animation.slice(0, 1).toUpperCase() + animation.slice(1) }
+                          </div>
+                        );
+                      })
+                    }
                   </div>
                 </div>
               </div>
-              <div id='stage-container'>
+              <div id='stage-container' className={this.state.isPresentationLoaded ? 'hidden' : ''}>
                 <div id='stage'>
-                  Drop a markdown file here to present or click here to reload the last one
+                  <span>Drop a markdown file here to start a presentation</span>
+                  <br />
+                  <span>OR</span>
+                  <br />
+                  <div className='control-button' onClick={this.reloadLastPresentation.bind(this)}>
+                    Reload the last presentation
+                  </div>
                 </div>
               </div>
-              <div id='presentation-container'>
+              <div id='presentation-container' className={!this.state.isPresentationLoaded ? 'hidden' : ''}>
                 <div id='presentation'>
 
                 </div>
