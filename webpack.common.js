a43c20c12c2e31e922bc5bb1c5ae4b4bf8da3816
Mohammed Ismail Ansari <team.terminal@gmail.com>
2018-06-12 20:27:54 +0000

    separate out babel configuration to .babelrc

--- webpack.common.js
+++ webpack.common.js
@@ -93,13 +93,7 @@ module.exports = {
                 test: /\.(jsx|js)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            'babel-preset-env',
-                            'babel-preset-react'
-                        ]
-                    }
+                    loader: 'babel-loader'
                 }
             },
             {
