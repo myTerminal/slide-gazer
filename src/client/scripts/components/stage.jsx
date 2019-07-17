34d6fe07a21b688f9092165c1d8e5e3faaaafb50
Mohammed Ismail Ansari <team.terminal@gmail.com>
2019-07-17 11:15:33 -0500

    update default URL for loading remote presentation

--- src/client/scripts/components/stage.jsx
+++ src/client/scripts/components/stage.jsx
@@ -73,7 +73,7 @@ export default class Stage extends React.Component {
                             });
                     }
                 },
-                defaultResponse: 'https://github.com/myTerminal/presentations/blob/master/why-use-emacs-in-2017/README.md',
+                defaultResponse: 'https://github.com/myTerminal/presentations/blob/master/technical/why-use-emacs-in-2017/README.md',
                 labels: [
                     'Load',
                     'Cancel'
